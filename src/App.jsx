33da38a454dd87ecab9f4080270c09dad85191f0
Gnössienne <91949315+hieutrung0451@users.noreply.github.com>
2022-12-02 13:56:14 +0700

    add custom form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
 
+// custom components
+import CustomForm from "./components/CustomForm";
+
 function App() {
-  return <div className="App"></div>;
+  const addTask = (task) => {
+    console.log(task);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={addTask} />
+    </div>
+  );
 }
 
 export default App;
