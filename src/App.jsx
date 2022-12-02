9219b3369a9c07d451e895c36a10fa5443fc5160
Gnössienne <91949315+hieutrung0451@users.noreply.github.com>
2022-12-02 14:52:09 +0700

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 
 // custom components
 import CustomForm from "./components/CustomForm";
+import TaskList from "./components/TaskList";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   const addTask = (task) => {
-    console.log(task);
+    setTasks((prevState) => [...prevState, task]);
   };
 
   return (
@@ -14,6 +17,7 @@ function App() {
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={addTask} />
+      {tasks && <TaskList tasks={tasks} />}
     </div>
   );
 }
