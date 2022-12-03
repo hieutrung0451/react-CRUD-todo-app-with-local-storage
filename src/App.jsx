b4e228b465935d07e641033bc40d08a864c3e24d
Gnössienne <91949315+hieutrung0451@users.noreply.github.com>
2022-12-03 15:55:38 +0700

    add toggle task

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,27 @@ function App() {
     setTasks((prevState) => prevState.filter((task) => task.id !== id));
   };
 
+  const toggleTask = (id) => {
+    setTasks((prevState) =>
+      prevState.map((task) =>
+        task.id === id ? { ...task, checked: !task.checked } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
       <CustomForm addTask={addTask} />
-      {tasks && <TaskList tasks={tasks} deleteTask={deleteTask} />}
+      {tasks && (
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+        />
+      )}
     </div>
   );
 }
